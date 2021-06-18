6a7f1c5fa55183b2254eb676c1a75d7bd70b6fdb
PS <pava.bore@gmail.com>
2021-06-18 22:19:14 +0300

    First version

---
+++ aTemplate/src/main/java/by/jrr/start/bean/Coin.java
@@ -0,0 +1,13 @@
+package by.jrr.start.bean;
+
+public class Coin {
+
+    public static final String oneSideCoin = "Eagle";
+    public static final String secondSideCoin = "Tails";
+    public static final String edgeCoin = "Edge";
+
+    @Override
+    public String toString() {
+        return "{}";
+    }
+}
