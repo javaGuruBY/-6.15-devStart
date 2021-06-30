c9417b1db6e596ad20fc3b185aa7d66c30659823
lamaconda <kennethoffi6.6@gmail.com>
2021-06-30 20:31:37 +0300

    adds CheckUserPassword functionality

--- homework/java/ilyaShelkovich/TDD_lecture_code_repeat/src/main/java/com/tutrit/java/quickstart/service/LoginService.java
+++ homework/java/ilyaShelkovich/TDD_lecture_code_repeat/src/main/java/com/tutrit/java/quickstart/service/LoginService.java
@@ -1,4 +1,9 @@
 package com.tutrit.java.quickstart.service;
 
+import com.tutrit.java.quickstart.bean.User;
+
 public class LoginService {
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }
--- homework/java/ilyaShelkovich/TDD_lecture_code_repeat/src/test/java/com/tutrit/java/quickstart/service/LoginServiceTest.java
+++ homework/java/ilyaShelkovich/TDD_lecture_code_repeat/src/test/java/com/tutrit/java/quickstart/service/LoginServiceTest.java
@@ -1,7 +1,46 @@
 package com.tutrit.java.quickstart.service;
 
-import static org.junit.Assert.*;
+import com.tutrit.java.quickstart.bean.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class LoginServiceTest {
+
+    LoginService loginService;
+
+    @Before
+    public void setUp(){
+        this.loginService = new LoginService();
+    }
+
+    @Test
+    public void checkUserPasswordPositive(){
+        User user = getUser();
+
+        String userInput = "password";
+
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void checkUserPasswordNegative(){
+        User user = getUser();
+
+        String userInput = "wrong";
+
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertFalse(actualResult);
+    }
+
+
+    private User getUser() {
+        User user = new User();
+        user.setPassword("password");
+        return user;
+    }
 
 }
