1de6f94a2cbccb6b05f061b2ac0c8692775c1363
lamaconda <kennethoffi6.6@gmail.com>
2021-06-30 20:47:47 +0300

    adds reduce login attempt on wrong password

--- homework/java/ilyaShelkovich/TDD_lecture_code_repeat/src/test/java/com/tutrit/java/quickstart/service/LoginServiceTest.java
+++ homework/java/ilyaShelkovich/TDD_lecture_code_repeat/src/test/java/com/tutrit/java/quickstart/service/LoginServiceTest.java
@@ -8,18 +8,18 @@ import org.junit.Test;
 public class LoginServiceTest {
 
     LoginService loginService;
+    User user;
 
     @Before
     public void setUp(){
         this.loginService = new LoginService();
+        this.user = getUser();
+        Assert.assertEquals(3, user.getLoginAttempts());
     }
 
     @Test
     public void checkUserPasswordPositive(){
-        User user = getUser();
-
         String userInput = "password";
-
 
         boolean actualResult = loginService.checkUserPassword(user, userInput);
         Assert.assertTrue(actualResult);
@@ -27,20 +27,38 @@ public class LoginServiceTest {
 
     @Test
     public void checkUserPasswordNegative(){
-        User user = getUser();
-
         String userInput = "wrong";
-
 
         boolean actualResult = loginService.checkUserPassword(user, userInput);
         Assert.assertFalse(actualResult);
     }
 
+    @Test
+    public void reduceLoginAttempts(){
+        loginService.reduceLoginAttempts(user);
+        Assert.assertEquals(2, user.getLoginAttempts());
+    }
+
+    @Test
+    public void loginPositive(){
+        String userInput = "password";
+
+        boolean actualResult = loginService.login(user, userInput);
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void loginNegative(){
+        String userInput = "wrong";
+
+        boolean actualResult = loginService.login(user, userInput);
+        Assert.assertFalse(actualResult);
+        Assert.assertEquals(2, user.getLoginAttempts());
+    }
 
     private User getUser() {
         User user = new User();
         user.setPassword("password");
         return user;
     }
-
 }
